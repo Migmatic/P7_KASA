d0979b1ff38de360e1f5edde11f152a124b853b2
migmatic <lamandealexis@gmail.com>
2023-02-27 06:57:29 +0100

    Responsive

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -17,8 +17,8 @@ function Collapse(props) {
     }
     else{
         return (
-            <div class="collapse">
-                <button class="collapse__button" onClick={() => setFold(true)}>
+            <div class="collapse collapse--unfolded">
+                <button class="collapse__button " onClick={() => setFold(true)}>
                     {props.title}
                     <svg width="24" height="15" viewBox="0 0 24 15" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M21.7268 14.1403L23.8597 11.9951L11.9298 0.0764155L-5.01125e-05 12.0072L2.13287 14.1403L11.9298 4.34257L21.7268 14.1403Z" fill="white"/>
